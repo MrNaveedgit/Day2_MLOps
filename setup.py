e753aefc720e448f45f3cd67841e0e96b61d945e
MrNaveedgit <naveed49478@gmail.com>
2021-10-14 05:09:54 +0500

    setup done

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version="0.0.1",
+    description="Its a MLOPs wine Q package",
+    author="Naveed Ahmed",
+    packages= find_packages(),
+    license="MIT"
+    
+)
+
+
